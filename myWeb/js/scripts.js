c02ad532d3f561e97767f7fbba2dce020097f4b4
hadoyyo <126799400+hadoyyo@users.noreply.github.com>
2025-06-17 16:03:14 +0200

    active section visibility bug fix

--- myWeb/js/scripts.js
+++ myWeb/js/scripts.js
@@ -12,13 +12,12 @@ window.addEventListener('DOMContentLoaded', event => {
     // Activate Bootstrap scrollspy on the main nav element
     const sideNav = document.body.querySelector('#sideNav');
     if (sideNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#sideNav',
-            rootMargin: '0px 0px -40%',
-        });
+       new bootstrap.ScrollSpy(document.body, {
+        target: '#sideNav',
+        rootMargin: '0px 0px 0px',
+    });
     };
 
-    // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
     const responsiveNavItems = [].slice.call(
         document.querySelectorAll('#navbarResponsive .nav-link')
